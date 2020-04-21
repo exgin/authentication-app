9053983d685cb35b697f97209f53b481cf1a220a
unknown <exgin3538@yahoo.com>
2020-04-20 21:30:13 -0400

    update requirements.txt & form website

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from models import connect_db, db,
+from models import connect_db, db
+from forms import LoginForm, RegisterForm
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgres:///feedback_db"
@@ -12,3 +13,15 @@ connect_db(app)
 db.create_all()
 
 toolbar = DebugToolbarExtension(app)
+
+@app.route('/')
+def goto_register():
+    """Meant to redirect user to /register"""
+    return redirect('/register')
+
+@app.route('/register', methods=["GET", "POST"])
+def register_page():
+    """Form for registering a user & handle this logic"""
+    form = RegisterForm()
+
+    return render_template('home-reg.html', form=form)
---
+++ forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Email
+
+
+class RegisterForm(FlaskForm):
+    """Form for registering a user"""
+
+    username = StringField("Username", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
+    first_name = StringField("First Name", validators=[InputRequired()])
+    last_name = StringField("Last Name", validators=[InputRequired()])
+    email = StringField("Email", validators=[InputRequired(), Email()])
+
+class LoginForm(FlaskForm):
+    """Form for registering a user"""
+
+    username = StringField("Username", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
